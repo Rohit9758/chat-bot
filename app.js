f24c370f484c0479de408b460ff755ec06a88fbc
Rohit9758 <rohit.bhure@gmail.com>
2022-07-07 18:25:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 9000);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.get('/', (req,res) => {
+res.send("Hello from Chat-Bot App");
+})
+
+try {
+  app.listen(app.get('port'), function () {
+      console.log('Listening on port 9000...');
+  })
+} catch(err){
+  console.log("Error in Dbconnection",err)
+}
